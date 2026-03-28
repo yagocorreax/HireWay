43374d7de822ba19fc900d2a2888f7d795e9b6ac
Yago Ferreira <yagocmferreira9@gmail.com>
2026-03-28 18:15:53 -0300

    Update app.py

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
+
+from src.api.ingestion.resume_parser import extract_text_from_pdf
 
 app = FastAPI(title="HireWay API")
 
+upload_dir= "data/raw/resumes"
+
 @app.get("/health")
 def health_check():
-    return{"status": "ok"}
+    return {"status": "ok"}
+
+
+@app.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    
+    file_path = os.path.join(upload_dir, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    
+    extracted_text = extract_text_from_pdf(file_path)
+
+    return {
+        "filename": file.filename,
+        "text_preview": extracted_text[:1000] 
+    }
