be1cb2f667af28af0751ad7e6692badd8f717759
Yago Ferreira <yagocmferreira9@gmail.com>
2026-04-06 20:24:07 -0300

    Update skill_extractor.py

--- src/api/processing/skill_extractor.py
+++ src/api/processing/skill_extractor.py
@@ -1,1 +1,11 @@
 from src.api.processing.skills_list import SKILLS
+
+def extract_skills(text: str):
+    text = text.lower()
+    found_skills = []
+
+    for skill in SKILLS:
+        if skill in text:
+            found_skills.append(skill)
+
+    return list(set(found_skills))
