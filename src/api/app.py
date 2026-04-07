02a4b0f092030b63e8a505663c911926e3155937
Yago Ferreira <yagocmferreira9@gmail.com>
2026-04-07 08:39:59 -0300

    Update app.py

--- src/api/app.py
+++ src/api/app.py
@@ -3,10 +3,14 @@ import shutil
 import os
 from src.api.processing.skill_extractor import extract_skills
 from src.api.ingestion.resume_parser import extract_text_from_pdf
+from src.api.processing.matcher import match_skills
 
 app = FastAPI(title="HireWay API")
 
-upload_dir= "data/raw/resumes"
+UPLOAD_DIR = "data/raw/resumes"
+
+# garante que a pasta existe
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/health")
 def health_check():
@@ -15,18 +19,43 @@ def health_check():
 @app.post("/upload-resume")
 async def upload_resume(file: UploadFile = File(...)):
     
-    file_path = os.path.join(upload_dir, file.filename)
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    # salvar arquivo
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    extracted_text = extract_text_from_pdf(file_path)
+
+    resume_skills = extract_skills(extracted_text)
+
+    return {
+        "filename": file.filename,
+        "skills": resume_skills,
+        "text_preview": extracted_text[:1000]
+    }
+
+@app.post("/analyze")
+async def analyze_resume(
+    file: UploadFile = File(...),
+    job_description: str = ""
+):
+    
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    
     extracted_text = extract_text_from_pdf(file_path)
 
-    skills = extract_skills(extracted_text)
+    resume_skills = extract_skills(extracted_text)
+    job_skills = extract_skills(job_description)
+
+    match_result = match_skills(resume_skills, job_skills)
 
     return {
-        "filename": file.filename,
-        "skills": skills,
-        "text_preview": extracted_text[:1000]
+        "resume_skills": resume_skills,
+        "job_skills": job_skills,
+        "match": match_result
     }
+
